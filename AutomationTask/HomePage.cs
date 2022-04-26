083d2757d25c9845e0f26728439d7be1c57f67cf
Saleh <saleh@msadats-imac.attlocal.net>
2022-04-25 22:25:22 -0400

    first commit

---
+++ AutomationTask/HomePage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+
+namespace AutomationTask
+{
+    public class HomePage
+    {
+        IWebDriver driver;
+        
+        public HomePage()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        
+        public void NavigateToHomePage()
+        {
+            driver.Navigate().GoToUrl("https://dotnetfiddle.net/");
+
+        }
+
+        public void Quit()
+        {
+            driver.Quit();
+
+        }
+
+        public void ClickOnRunButton()
+        {
+            Thread.Sleep(1000);
+
+            driver.FindElement(By.Id("run-button")).Click();
+        }
+
+        public string GetStringFromOutput()
+        {
+            IWebElement outputText = driver.FindElement(By.Id("output"));
+            String output = outputText.Text;
+            return output;
+        }
+
+        public void ClickOnSaveButton()
+        {
+            Thread.Sleep(1000);
+
+            driver.FindElement(By.Id("save-button")).Click();
+        }
+
+        public void VerifyLoginWindowAppeared()
+        {
+            IWebElement outputText = driver.FindElement(By.Id("Email"));
+            String output = outputText.GetAttribute("name");
+            //Verifying Login Windown Appeared
+            Assert.AreEqual(output, "Email");
+        }
+    }
+}
---
+++ AutomationTask/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AutomationTask
+{
+    public class Tests
+    {
+        HomePage homePage;
+        string expectedOutput = "Hello World";
+
+        [SetUp]
+        public void Setup()
+        {
+            homePage = new HomePage();
+
+            homePage.NavigateToHomePage();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            homePage.ClickOnRunButton();
+            
+            Assert.AreEqual(homePage.GetStringFromOutput(), expectedOutput);
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            homePage.ClickOnSaveButton();
+
+            homePage.VerifyLoginWindowAppeared();
+            Assert.Pass();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            homePage.Quit();
+        }
+    }
+}
